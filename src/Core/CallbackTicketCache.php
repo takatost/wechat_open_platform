16edacb5c3ecaa97efd43dafb18577a330e58cec
takatost <takatost@gmail.com>
2016-08-09 20:37:28 +0800

    Add CallbackTicketCache

---
+++ src/Core/CallbackTicketCache.php
@@ -0,0 +1,102 @@
+<?php
+/*
+ * This file is part of the takatost/wechat_open_platform.
+ *
+ * (c) takatost <takatost@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Changyi\Library\WeixinFw;
+
+use Doctrine\Common\Cache\CacheProvider;
+
+class CallbackTicketCache extends CacheProvider
+{
+    protected $fetchCallback;
+    protected $saveCallback;
+
+    public function setFetchCallback($callback)
+    {
+        $this->fetchCallback = $callback;
+    }
+
+    public function setSaveCallback($callback)
+    {
+        $this->saveCallback = $callback;
+    }
+
+    /**
+     * Fetches an entry from the cache.
+     *
+     * @param string $id The id of the cache entry to fetch.
+     *
+     * @return mixed|false The cached data or FALSE, if no cache entry exists for the given id.
+     */
+    protected function doFetch($id)
+    {
+        return call_user_func($this->fetchCallback);
+    }
+
+    /**
+     * Tests if an entry exists in the cache.
+     *
+     * @param string $id The cache id of the entry to check for.
+     *
+     * @return bool TRUE if a cache entry exists for the given cache id, FALSE otherwise.
+     */
+    protected function doContains($id)
+    {
+        return true;
+    }
+
+    /**
+     * Puts data into the cache.
+     *
+     * @param string $id         The cache id.
+     * @param string $data       The cache entry/data.
+     * @param int    $lifeTime   The lifetime. If != 0, sets a specific lifetime for this
+     *                           cache entry (0 => infinite lifeTime).
+     *
+     * @return bool TRUE if the entry was successfully stored in the cache, FALSE otherwise.
+     */
+    protected function doSave($id, $data, $lifeTime = 0)
+    {
+        return call_user_func_array($this->saveCallback, [$data]);
+    }
+
+    /**
+     * Deletes a cache entry.
+     *
+     * @param string $id The cache id.
+     *
+     * @return bool TRUE if the cache entry was successfully deleted, FALSE otherwise.
+     */
+    protected function doDelete($id)
+    {
+        return true;
+    }
+
+    /**
+     * Flushes all cache entries.
+     *
+     * @return bool TRUE if the cache entries were successfully flushed, FALSE otherwise.
+     */
+    protected function doFlush()
+    {
+        return true;
+    }
+
+    /**
+     * Retrieves cached information from the data store.
+     *
+     * @since 2.2
+     *
+     * @return array|null An associative array with server's statistics if available, NULL otherwise.
+     */
+    protected function doGetStats()
+    {
+        return [];
+    }
+}
